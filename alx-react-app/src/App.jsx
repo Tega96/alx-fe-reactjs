3b2af4ef631ff1c20f43b435db8f2c0499dd2bde
Otega Otite <otiteotega@gmail.com>
2026-01-11 09:12:48 +0100

    Create a User Profile card using props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,9 @@ import Header from './Header'
 import MainContent from './MainContent'
 import WelcomeMessage from './components/WelcomeMessage'
 import Footer from './Footer'
+import UserProfile from './components/UserProfile'
+
+
 const App = () => {
 
   return (
@@ -9,6 +12,7 @@ const App = () => {
       <Header />
       <MainContent />
       <WelcomeMessage />
+      <UserProfile name='Alice' age='25' bio='Loves hiking and photography' />
       <Footer />
     </div>
   )
