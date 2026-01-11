923f049a93e30185269926c2e1febb1c52d57414
Otega Otite <otiteotega@gmail.com>
2026-01-11 09:07:10 +0100

    create specific components in a react application

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,15 @@
+import Header from './Header'
+import MainContent from './MainContent'
 import WelcomeMessage from './components/WelcomeMessage'
-
+import Footer from './Footer'
 const App = () => {
 
   return (
     <div>
+      <Header />
+      <MainContent />
       <WelcomeMessage />
+      <Footer />
     </div>
   )
 }
