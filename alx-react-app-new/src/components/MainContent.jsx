7acf3f840d174160f2d7fa5416d4ae34a469afb2
Otega Otite <otiteotega@gmail.com>
2026-01-11 22:50:29 +0100

    Apply inline Css Styling to React components

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,7 +2,7 @@
 const MainContent = () => {
     return (
         <main style={{ height: '50vh'}}>
-            <p style={{ color: 'gray', fontWeight: 'bold', padding: '10px'}}>I love to visit New York, Paris, and Tokyo</p>
+            <p style={{ color: 'gray', fontWeight: 'bold', padding: '10px'}}>"I love to visit New York, Paris, and Tokyo."</p>
         </main>
     )
 }
