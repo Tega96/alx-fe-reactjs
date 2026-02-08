bf3b6d94c65b0541c1d6b15e83cc665603fb99a6
Otega Otite <otiteotega@gmail.com>
2026-02-08 23:53:46 +0100

    Adding animation to component with tailwindcss

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,11 +1,11 @@
 
 const UserProfile = () => {
     return (
-        <div className="bg-gray-100 p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-            <img src="https://via.placeholder.com/150" alt="User" 
-                className="w-24 md:w-36 h-24 md:h-36 rounded-full mx-auto"
+        <div className="bg-gray-100 p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl">
+            <img src="/vite.svg" alt="User" 
+                className="w-24 md:w-36 h-24 md:h-36 rounded-full mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
             />
-            <h1 className="text-sm md:text-xl text-blue-800 my-4">John Doe</h1>
+            <h1 className="text-sm md:text-xl text-blue-800 hover:text-blue-500 my-4">John Doe</h1>
             <p className="text-gray-600 text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
         </div>
     )
