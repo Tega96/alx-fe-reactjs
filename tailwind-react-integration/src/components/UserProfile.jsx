9e0b5c239ba95658144fa6f73b85fd1065d1036c
Otega Otite <otiteotega@gmail.com>
2026-02-08 23:57:28 +0100

    Responsive Design implementation using tailwind

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 
 const UserProfile = () => {
     return (
-        <div className="bg-gray-100 p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl">
+        <div className="bg-gray-100 sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl">
             <img src="/vite.svg" alt="User" 
-                className="w-24 md:w-36 h-24 md:h-36 rounded-full mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
+                className="sm:w-24 md:w-36 sm:h-24 md:h-36 rounded-full mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
             />
-            <h1 className="text-sm md:text-xl text-blue-800 hover:text-blue-500 my-4">John Doe</h1>
-            <p className="text-gray-600 text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+            <h1 className="sm:text-sm md:text-xl text-blue-800 hover:text-blue-500 my-4">John Doe</h1>
+            <p className="text-gray-600 sm:text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
         </div>
     )
 }
