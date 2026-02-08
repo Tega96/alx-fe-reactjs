f9c036a20e7cf2539d4243ccd83c9b6a0665dd9b
Otega Otite <otiteotega@gmail.com>
2026-02-09 00:21:04 +0100

    Responsive Design Implementation Using Tailwind CSS

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -5,7 +5,7 @@ const UserProfile = () => {
             <img src="/vite.svg" alt="User" 
                 className="sm:w-24 md:w-36 sm:h-24 md:h-36 rounded-full mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
             />
-            <h1 className="sm:text-sm md:text-xl text-blue-800 hover:text-blue-500 my-4">John Doe</h1>
+            <h1 className="sm:text-lg md:text-xl text-blue-800 hover:text-blue-500 my-4">John Doe</h1>
             <p className="text-gray-600 sm:text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
         </div>
     )
